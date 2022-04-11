a6ae0118f6944d3017f03217119e5932242a76a8
NiyiMorino <61976166+NiyiMorino@users.noreply.github.com>
2022-04-11 19:27:14 +0900

    Add files via upload

---
+++ ReserveConfirm.java
@@ -0,0 +1,14 @@
+package edu.handong.android.soccerfield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReserveConfirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_confirm);
+    }
+}
