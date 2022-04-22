cf8ed43555523d255c37474bc14450d32f2961d7
Omoniyi Omotola <niyi.omotola@gmail.com>
2022-04-22 11:54:29 +0900

    create app

---
+++ app/src/main/java/edu/handong/android/soccerfield/AdminOptions.java
@@ -0,0 +1,14 @@
+package edu.handong.android.soccerfield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_options);
+    }
+}
