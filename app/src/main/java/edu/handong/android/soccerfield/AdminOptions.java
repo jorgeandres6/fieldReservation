49ece2b96977ac7f1d0827a84c1d7521a49098e6
NiyiMorino <61976166+NiyiMorino@users.noreply.github.com>
2022-04-25 20:51:04 +0900

    update 2

--- app/src/main/java/edu/handong/android/soccerfield/AdminOptions.java
+++ app/src/main/java/edu/handong/android/soccerfield/AdminOptions.java
@@ -10,5 +10,5 @@ public class AdminOptions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_options);
-    }
+    }  //test
 }
