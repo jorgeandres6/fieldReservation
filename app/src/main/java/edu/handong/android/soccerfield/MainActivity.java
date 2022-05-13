a1188246d5a87d6f02f9344bfa703f2779be9b5f
Jorge Maldonado <jorgeandres611@gmail.com>
2022-05-13 17:59:50 +0900

    Firebase connection succes

--- app/src/main/java/edu/handong/android/soccerfield/MainActivity.java
+++ app/src/main/java/edu/handong/android/soccerfield/MainActivity.java
@@ -2,7 +2,9 @@ package edu.handong.android.soccerfield;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button navigate_btn = findViewById(R.id.CheckField);
+        navigate_btn.setOnClickListener(view -> {
+            Intent intent = new Intent(this,SF_list.class);
+            startActivity(intent);
+        });
+
     }
+
+
 }
--- app/src/main/java/edu/handong/android/soccerfield/SF_list.java
+++ app/src/main/java/edu/handong/android/soccerfield/SF_list.java
@@ -1,14 +1,66 @@
 package edu.handong.android.soccerfield;
 
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class SF_list extends AppCompatActivity {
+
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    StorageReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sf_list);
+
+        Button navigate_btn = findViewById(R.id.button);
+        navigate_btn.setOnClickListener(view -> {
+            getFields();
+        });
+
+    }
+
+    public void getFields (){
+        storageReference = firebaseStorage.getInstance().getReference();
+        ref = storageReference.child("LOFF.png");
+
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url = uri.toString();
+                dwFile(SF_list.this,"LOFF","png",DIRECTORY_DOWNLOADS,url);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    public void dwFile (Context context, String fileName, String fileExtension, String desDirectory, String url){
+        DownloadManager dwMngr = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, desDirectory,fileName+fileExtension);
+
+        dwMngr.enqueue(request);
     }
 }
