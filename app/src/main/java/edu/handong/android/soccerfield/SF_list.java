e322d679f220b8c3515506fb8e3793199129d0bf
Jorge Maldonado <jorgeandres611@gmail.com>
2022-05-16 18:27:18 +0900

    Image retrieving done and working with inflating

--- app/src/main/java/edu/handong/android/soccerfield/SF_list.java
+++ app/src/main/java/edu/handong/android/soccerfield/SF_list.java
@@ -1,27 +1,30 @@
 package edu.handong.android.soccerfield;
 
-import static android.os.Environment.DIRECTORY_DOWNLOADS;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DownloadManager;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
+import com.squareup.picasso.Picasso;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
 
 public class SF_list extends AppCompatActivity {
 
-    FirebaseStorage firebaseStorage;
-    StorageReference storageReference;
-    StorageReference ref;
+    LinearLayout LL;
+    String [] nombres = {"1","2","3"};
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +32,46 @@ public class SF_list extends AppCompatActivity {
         setContentView(R.layout.activity_sf_list);
 
         Button navigate_btn = findViewById(R.id.button);
+        ImageView rImage = findViewById(R.id.imgPrueba);
+        LL = findViewById(R.id.LinL);
+        i = 0;
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        DatabaseReference getImage = databaseReference.child("canchas").child("quito").child("1").child("fields").child("0").child("img");
+
+        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // getting a DataSnapshot for the location at the specified
+                // relative path and getting in the link variable
+                String link = dataSnapshot.getValue(String.class);
+
+                // loading that data into rImage
+                // variable which is ImageView
+                Picasso.get().load(link).into(rImage);
+            }
+
+            // this will called when any problem
+            // occurs in getting data
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // we are showing that error message in toast
+                Toast.makeText(SF_list.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         navigate_btn.setOnClickListener(view -> {
-            getFields();
-        });
-
-    }
-
-    public void getFields (){
-        storageReference = firebaseStorage.getInstance().getReference();
-        ref = storageReference.child("LOFF.png");
-
-        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                String url = uri.toString();
-                dwFile(SF_list.this,"LOFF","png",DIRECTORY_DOWNLOADS,url);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-            }
+            add(nombres[i]);
+            i++;
         });
     }
 
-    public void dwFile (Context context, String fileName, String fileExtension, String desDirectory, String url){
-        DownloadManager dwMngr = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(url);
-        DownloadManager.Request request = new DownloadManager.Request(uri);
-
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalFilesDir(context, desDirectory,fileName+fileExtension);
-
-        dwMngr.enqueue(request);
+    private void add(String n){
+        View v = getLayoutInflater().inflate(R.layout.card,null);
+        Button b = v.findViewById(R.id.button3);
+        b.setText(n);
+        LL.addView(v);
     }
+
 }
