f28ca3f22248e0f2cfc676567c993f11dfc8f410
NiyiMorino <61976166+NiyiMorino@users.noreply.github.com>
2022-06-02 11:34:44 +0900

    File 02_06

--- app/src/main/java/edu/handong/android/soccerfield/MainActivity.java
+++ app/src/main/java/edu/handong/android/soccerfield/MainActivity.java
@@ -1,8 +1,14 @@
 package edu.handong.android.soccerfield;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,7 +32,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_main);
+
+        Button changeLang = findViewById(R.id.changeLang);
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //show AlertDialog to display list of languages, one can be selected
+                showChangeLanguageDialog();
+            }
+        });
 
        autoCompleteText = findViewById(R.id.auto_complete_txt);
 
@@ -72,6 +88,54 @@ public class MainActivity extends AppCompatActivity {
            }
        });
 
+
+    }
+
+    private void showChangeLanguageDialog() {
+        //array of languages on menu
+        final String[] listItems = {"English", "Español"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setTitle("Choose Language...");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i == 0) {
+                    setLocale("esp");
+                    recreate();
+                }
+                else if (i == 1) {
+                    setLocale("en");
+                    recreate();
+                }
+                else if (i == 2) {
+                    setLocale("kr");
+                    recreate();
+                }
+
+                dialogInterface.dismiss();
+            }
+
+        });
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale= new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config= new Configuration();
+        config.locale=locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor=getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences prefs=getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language=prefs.getString("My_Lang", "");
+        setLocale(language);
 
     }
 
